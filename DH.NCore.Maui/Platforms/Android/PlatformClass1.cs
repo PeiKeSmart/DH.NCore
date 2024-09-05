b3786e172ac1156a7cddcd7aad13619c11377ddc
猿人易 <qcjxberin@outlook.com>
2024-09-05 11:28:15 +0800

    Revert "增加net9的支持及Maui类库"
    This reverts commit 418d3f066cac684838cefe630159af161c7aa579.

--- DH.NCore.Maui/Platforms/Android/PlatformClass1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DH.NCore.Maui;
-
-// All the code in this file is only included on Android.
-public class PlatformClass1
-{
-}
--- DH.NCore.Maui/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DH.NCore.Maui;
-
-// All the code in this file is only included on Windows.
-public class PlatformClass1
-{
-}
