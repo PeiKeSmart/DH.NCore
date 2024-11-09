465b5a75fa4021baf936806357b2eb6f88bdd0ca
猿人易 <qcjxberin@outlook.com>
2024-11-09 10:11:03 +0800

    按照系统约定，includeBaseDirectory默认值使用false，同时改进压缩代码，优先使用高版本已有API。

--- XUnitTest.Core/IO/PathHelperTests.cs
+++ XUnitTest.Core/IO/PathHelperTests.cs
@@ -67,4 +67,26 @@ public class PathHelperTests
         di2.Refresh();
         Assert.True(di2.Exists);
     }
+
+    [Fact]
+    public void DirectoryCompress3()
+    {
+        var dst = "alg3.zip".AsFile();
+        var src = "Algorithms".AsDirectory();
+
+        if (dst.Exists) dst.Delete();
+
+        src.Compress(dst.FullName, true);
+
+        dst.Refresh();
+        Assert.True(dst.Exists);
+
+        var di2 = "Algorithms3".AsDirectory();
+        if (di2.Exists) di2.Delete(true);
+
+        dst.Extract(di2.FullName, true);
+
+        di2.Refresh();
+        Assert.True(di2.Exists);
+    }
 }
